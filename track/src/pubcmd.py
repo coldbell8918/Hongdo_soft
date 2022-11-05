1fc72784713f2b2fd45a46c3431b64deb052f542
coldbell8918 <pjc0525@hanyang.ac.kr>
2022-11-05 23:28:25 +0900

    jc1

--- track/src/pubcmd.py
+++ track/src/pubcmd.py
@@ -1,86 +1,52 @@
 #!/usr/bin/env python3
-from ast import Global
-from turtle import position
-import time
-import rospy
-from std_msgs.msg import String
-from std_msgs.msg import Float64MultiArray  
+
+import rospy 
 from std_msgs.msg import Bool
 from sol_msg.msg import track
 from geometry_msgs.msg import Twist
-from std_srvs.srv import Trigger, TriggerResponse
-from track.srv import PlaySong, PlaySongResponse
-import os
 
+class human_tracker():
+    def __init__(self):
+        self.stop = True
+        self.subs1=[]
+        self.subs2=[]
+        self.pubs=[]
+        self.subs1.append(rospy.Subscriber('stop', Bool, self.stop_callback))
+        self.subs2.append(rospy.Subscriber('tracker', track, self.tracker_callback))
+        self.pubs['cmd_vel'] = rospy.Publisher("cmd_vel", Twist,queue_size=1)
 
-stop = 1
-cnt=0
+    def stop_callback(self, data):
+        if data.data == True:
+            self.stop=True
+        else:
+            self.stop = False
 
-def tracker_callback(data):
-    global cnt
-    play_song_client(1)
-    if (stop):
-        rospy.loginfo('stop')
-        cmd(0, 0)
-    else:
+    def tracker_callback(self, data):
+        if self.stop: return
+        
         if data.position[0] <260:
             rospy.loginfo('turn right')
-            cmd(0, 0.2) 
+            self.cmd(0, 0.1) 
 
         elif data.position[0] <410:
             rospy.loginfo('go')
-            cmd(0.2, 0)
-    
+            self.cmd(0.1, 0)
+
         else:
             rospy.loginfo('turn left')
-<<<<<<< HEAD
-            cmd(0, -0.2)
-=======
-            cmd(0, -0.1)
-    cnt+=1
-    if cnt==1000:
-        cnt=0
+            self.cmd(0, -0.1)
 
-
-
-def play_song_client(number):
-    rospy.wait_for_service('play_voice')
-    try:
-        play_song = rospy.ServiceProxy('play_voice',PlaySong)
-        return play_song(number)
-    except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
-    
->>>>>>> 512ff3f0cd1087654c765f70c757586bb2b3894a
-
-
-def stop_callback(data):
-    global stop
-    if data.data == True:
-        stop = 1
-    else:
-        stop = 0
-        
-
-
-def init():
-    rospy.Subscriber('stop', Bool, stop_callback)
-    rospy.Subscriber('tracker', track, tracker_callback)
-    rospy.spin()
-
-
-def cmd(linear, angular):
-    pub=rospy.Publisher("cmd_vel", Twist, queue_size=10)
-
-    cmd_msg=Twist()
-    cmd_msg.linear.x=linear
-    cmd_msg.linear.y=0
-    cmd_msg.linear.z=0
-    cmd_msg.angular.x=0
-    cmd_msg.angular.y=0
-    cmd_msg.angular.z=angular
-    pub.publish(cmd_msg)
+    def cmd(self, linear, angular):
+        cmd_msg=Twist()
+        cmd_msg.linear.x=linear
+        cmd_msg.linear.y=0
+        cmd_msg.linear.z=0
+        cmd_msg.angular.x=0
+        cmd_msg.angular.y=0
+        cmd_msg.angular.z=angular
+        self.pubs['cmd_vel'].publish(cmd_msg)
     
 if __name__ == '__main__':
     rospy.init_node('pubcmd', anonymous=True)
-    init()
+    cls_=human_tracker()
+    rospy.spin()
